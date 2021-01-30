093b294018819b1800e6a219fac562e34d9cb25d
truongtmnt <61923039+truongtmnt@users.noreply.github.com>
2021-01-30 17:55:08 +1100

    add reselect, build selector for cart Item and count

--- src/components/card-icon/card-icon.component.jsx
+++ src/components/card-icon/card-icon.component.jsx
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 
-const CardIcon = ({ toggleCartHidden }) => {
+const CardIcon = ({ toggleCartHidden, itemCount }) => {
 	return (
 		<div className='cart-icon' onClick={toggleCartHidden}>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{itemCount}</span>
 		</div>
 	);
 };
@@ -16,5 +17,8 @@ const CardIcon = ({ toggleCartHidden }) => {
 const mapDispatchToProps = (dispatch) => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+const mapStateToProps = (state) => ({
+	itemCount: selectCartItemsCount(state),
+});
 
-export default connect(null, mapDispatchToProps)(CardIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CardIcon);
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
-
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 const CartDropdown = ({ cartItems }) => {
 	return (
@@ -18,9 +18,8 @@ const CartDropdown = ({ cartItems }) => {
 	);
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	cartItems,
+const mapStateToProps = (state) => ({
+	cartItems: selectCartItems(state),
 });
-
 
 export default connect(mapStateToProps)(CartDropdown);
