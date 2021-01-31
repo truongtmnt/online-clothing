58107c7dafcdc0f7d5f2af856dc47f709b4d79bc
truongtmnt <61923039+truongtmnt@users.noreply.github.com>
2021-01-31 15:42:43 +1100

    add clear, remove, add item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const initialState = {
 	hidden: true,
@@ -12,6 +12,16 @@ const cartReducer = (state = initialState, { type, payload }) => {
 			return { ...state, hidden: !state.hidden };
 		case CartActionTypes.ADD_ITEM:
 			return { ...state, cartItems: addItemToCart(state.cartItems, payload) };
+		case CartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state,
+				cartItems: state.cartItems.filter((cartItem) => cartItem !== payload),
+			};
+		case CartActionTypes.REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: removeItemFromCart(state.cartItems, payload),
+			};
 		default:
 			return state;
 	}
