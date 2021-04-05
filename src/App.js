42529b6070b314f7c4738f8a485dace493cdf42a
truongtmnt <61923039+truongtmnt@users.noreply.github.com>
2021-04-05 17:32:31 +1000

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import React, { Component } from "react";
 import "./App.css";
 
-import HomePage from "./pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import Checkout from "./pages/checkout/checkout.component.jsx";
